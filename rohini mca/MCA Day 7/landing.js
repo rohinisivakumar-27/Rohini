425b6878f1c921563c8d4bf71537a55463588ef0
rohinisivakumar-27 <rohinisivakumar27@gmail.com>
2025-08-23 20:49:46 +0530

    Add files via upload
    commmiting the project

---
+++ rohini mca/MCA Day 7/landing.js
@@ -0,0 +1,13 @@
+let iddom=document.getElementById('hello');
+let classdom=document.getElementsByClassName('box');
+let tagdom=document.getElementsByTagName('h2');
+let queseldom=document.querySelector('h2');
+
+
+console.log("ID SELECTION");
+console.log(iddom);
+
+console.log("CLASS SELECTOR");
+console.log(tagdom);
+
+console.log("Query selector");
